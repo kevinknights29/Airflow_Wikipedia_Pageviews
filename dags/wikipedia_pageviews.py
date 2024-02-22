95838e8202b5194ecb407c680255b20e4fc6e784
Kevin Knights <kevinknights29@hotmail.com>
2024-02-22 05:56:22 +0000

    refactor: added timezone aware task

--- dags/wikipedia_pageviews.py
+++ dags/wikipedia_pageviews.py
@@ -6,15 +6,17 @@ import pendulum
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+tz = "America/Panama"
+local_tz = pendulum.timezone(tz)
 dag = DAG(
     dag_id="wikipedia_pageviews",
-    start_date=pendulum.datetime(2024, 1, 1, tz="America/Panama"),
+    start_date=pendulum.datetime(2024, 1, 1, tz=local_tz),
     schedule_interval=None,
 )
 
 
 def _get_data(execution_date):
-    year, month, day, hour, *_ = execution_date.timetuple()
+    year, month, day, hour, *_ = local_tz.convert(execution_date).timetuple()
     url = (
         "https://dumps.wikimedia.org/other/pageviews/"
         f"{year}/{year}-{month:0>2}/"
