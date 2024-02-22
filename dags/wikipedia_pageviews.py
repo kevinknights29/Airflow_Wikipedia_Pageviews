dc02fc3a8c324babd29b522b15d20fccb54f5b3d
Kevin Knights <kevinknights29@hotmail.com>
2024-02-22 05:43:30 +0000

    refactor: updated BashOperator with PythonOperator

--- dags/wikipedia_pageviews.py
+++ dags/wikipedia_pageviews.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+from urllib import request
+
 import pendulum
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
 dag = DAG(
     dag_id="wikipedia_pageviews",
@@ -10,18 +12,20 @@ dag = DAG(
     schedule_interval=None,
 )
 
-get_data = BashOperator(
-    task_id="get_data",
-    bash_command=(
-        "curl -o /tmp/wikipageview.gz "
+
+def _get_data(execution_date):
+    year, month, day, hour, *_ = execution_date.timetuple()
+    url = (
         "https://dumps.wikimedia.org/other/pageviews/"
-        "{{ execution_date.year }}/"
-        "{{ execution_date.year }}-"
-        "{{ '{:02}'.format(execution_date.month) }}/"
-        "pageviews-{{ execution_date.year }}"
-        "{{ '{:02}'.format(execution_date.month) }}"
-        "{{ '{:02}'.format(execution_date.day) }}-"
-        "{{ '{:02}'.format(execution_date.hour) }}0000.gz"
-    ),
+        f"{year}/{year}-{month:0>2}/"
+        f"pageviews-{year}{month:0>2}{day:0>2}-{hour:0>2}0000.gz"
+    )
+    output_path = "/tmp/wikipageview.gz"
+    request.urlretrieve(url, output_path)
+
+
+get_data = PythonOperator(
+    task_id="get_data",
+    python_callable=_get_data,
     dag=dag,
 )
