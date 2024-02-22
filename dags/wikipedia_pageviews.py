ab931ad282a54f235badf7d7805ca0fe106d93fd
Kevin Knights <kevinknights29@hotmail.com>
2024-02-22 05:28:31 +0000

    feat: added dag

---
+++ dags/wikipedia_pageviews.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+dag = DAG(
+    dag_id="wikipedia_pageviews",
+    start_date=pendulum.Date("2024", "1", "1"),
+    schedule_interval=None,
+)
+
+get_data = BashOperator(
+    task_id="get_data",
+    bash_command=(
+        "curl -o /tmp/wikipageview.gz"
+        "https://dumps.wikimedia.org/other/pageviews/"
+        "{{ execution_date.year }}/"
+        "{{ execution_date.year }}-"
+        "{{ '{:02}'.format(execution_date.month) }}/"
+        "pageviews-{{ execution_date.year }}"
+        "{{ '{:02}'.format(execution_date.month) }}"
+        "{{ '{:02}'.format(execution_date.day) }}-"
+        "{{ '{:02}'.format(execution_date.hour) }}0000.gz"
+    ),
+    dag=dag,
+)
